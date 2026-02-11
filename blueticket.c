87b0415c524427cc1a8140d2d6485e9d15ec8a54
rishi8688 <rishi43341b@gmail.com>
2026-02-11 00:25:00 -0800

    Add files via upload

---
+++ blueticket.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void main() {
+    int a,b,c;
+    printf("enter the value in A:");
+    scanf("%d",&a);
+    printf("enter the value in B:");
+    scanf("%d",&b);
+    printf("enter the value in C:");
+    scanf("%d",&c);
+    int ab=a+b;
+    int bc=b+c;
+    int ac=a+c;
+    if (ab==10||bc==10||ac==10)
+    {
+        printf("10");
+    }
+    else if(a>10||b>10||c>10)
+    {
+        printf("5");
+    }
+    else
+    {
+        printf("0");
+    }
+}                                                                                                                           
