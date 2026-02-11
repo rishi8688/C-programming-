beaad908319bee9f05531af74c820d316ba69527
rishi8688 <rishi43341b@gmail.com>
2026-02-11 00:23:18 -0800

    Add files via upload

---
+++ redticket.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main() {
+    int a,b,c;
+    printf("enter the value in A:");
+    scanf("%d",&a);
+    printf("enter the value in B:");
+    scanf("%d",&b);
+    printf("enter the value in C:");
+    scanf("%d",&c);
+    if(a==2&&b==2&&c==2)
+    {
+        printf("10");
+    }
+    else if (a==b&&b==c&&c==a)
+    {
+        printf("5");
+    }
+    else if(b!=a&&c!=a)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("0");
+    }
+}
